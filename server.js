8604752d99ba9b2028c9b33cb54aff13dec85d10
Артем Фролов <artem.frolov3@leroymerlin.ru>
2024-10-16 22:37:51 +0300

    enable cors

--- server.js
+++ server.js
@@ -9,6 +9,14 @@ const router = jsonServer.router(path.resolve(__dirname, 'db.json'));
 server.use(jsonServer.defaults({}));
 server.use(jsonServer.bodyParser);
 
+// Add custom middleware for CORS
+server.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // Allow any origin
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+  });
+
 // Эндпоинт для логина
 server.post('/login', (req, res) => {
     try {
